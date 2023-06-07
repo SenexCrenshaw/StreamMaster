99c186f4235c4eb88b2886d1d203e245f67876e5
SenexCrenshaw <35600301+SenexCrenshaw@users.noreply.github.com>
2023-06-07 14:22:59 -0400

    Bump version

--- StreamMasterDomain/Dto/SettingDto.cs
+++ StreamMasterDomain/Dto/SettingDto.cs
@@ -9,5 +9,5 @@ public class SettingDto : Setting, IMapFrom<Setting>
     /// <summary>
     /// The version of the StreamMaster application.
     /// </summary>
-    public string Version { get; set; } = "0.1.7";
+    public string Version { get; set; } = "0.1.9";
 }
