f27f1f5db13670a7be260249a50517895b022e53
SenexCrenshaw <35600301+SenexCrenshaw@users.noreply.github.com>
2023-06-13 15:13:12 -0400

    🔖 chore(Release_Drafter.yml): remove branch filter and add workflow_dispatch trigger 🔖 chore(SettingDto.cs): update version to 0.2.10 The Release Drafter workflow has been updated to remove the branch filter and add the workflow_dispatch trigger to allow for manual triggering of the workflow. The SettingDto version has been updated to 0.2.10.

--- StreamMasterDomain/Dto/SettingDto.cs
+++ StreamMasterDomain/Dto/SettingDto.cs
@@ -9,5 +9,5 @@ public class SettingDto : Setting, IMapFrom<Setting>
     /// <summary>
     /// The version of the StreamMaster application.
     /// </summary>
-    public string Version { get; set; } = "0.2.9";
+    public string Version { get; set; } = "0.2.10";
 }
