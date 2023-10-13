1894519495962c5f792d30041ebc5a0fd18e3ce9
SenexCrenshaw <35600301+SenexCrenshaw@users.noreply.github.com>
2023-10-13 08:04:16 -0400

    Docker build update

--- build_docker.ps1
+++ build_docker.ps1
@@ -2,3 +2,9 @@
 $version = Get-Content "Version"
 $dockerCommand = ".\build_docker.bat"
 &$dockerCommand $version
+
+$env:DOCKER_BUILDKIT = 1
+$env:COMPOSE_DOCKER_CLI_BUILD = 1
+docker build --pull --rm -f "Dockerfile" -t streammaster:latest "."
+
+docker buildx build --platform linux/amd64,linux/arm64 -t docker.io/senexcrenshaw/streammaster:test -f ./Dockerfile . --push
