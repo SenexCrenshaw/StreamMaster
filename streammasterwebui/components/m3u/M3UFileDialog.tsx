3edfa006c1c5e8fe4fbf60e93017589ea236b571
SenexCrenshaw <35600301+SenexCrenshaw@users.noreply.github.com>
2023-10-13 20:32:25 -0400

    Fix ups

--- streammasterwebui/components/m3u/M3UFileDialog.tsx
+++ streammasterwebui/components/m3u/M3UFileDialog.tsx
@@ -1,4 +1,3 @@
-import { M3UFileStreamUrlPrefix } from '@/lib/common/streammaster_enums';
 import { useM3UFilesCreateM3UFileMutation, type CreateM3UFileRequest } from '@lib/iptvApi';
 import React, { useState } from 'react';
 import FileDialog, { type FileDialogProps } from '../sharedEPGM3U/FileDialog';
@@ -8,7 +7,7 @@ const M3UFileDialog: React.FC<Partial<FileDialogProps>> = () => {
 
   const [M3UFilesCreateM3UFileMutation] = useM3UFilesCreateM3UFileMutation();
 
-  const createM3U = async (name: string, source: string, maxStreams: number, startingChannelNumber: number, streamURLPrefix: M3UFileStreamUrlPrefix) => {
+  const createM3U = async (name: string, source: string, maxStreams: number, startingChannelNumber: number) => {
     const addM3UFileRequest = {} as CreateM3UFileRequest;
 
     addM3UFileRequest.name = name;
@@ -16,7 +15,7 @@ const M3UFileDialog: React.FC<Partial<FileDialogProps>> = () => {
     addM3UFileRequest.urlSource = source;
     addM3UFileRequest.maxStreamCount = maxStreams;
     addM3UFileRequest.startingChannelNumber = startingChannelNumber;
-    addM3UFileRequest.streamURLPrefixInt = parseInt(streamURLPrefix?.toString() ?? '0');
+    // addM3UFileRequest.streamURLPrefixInt = parseInt(streamURLPrefix?.toString() ?? '0');
 
     await M3UFilesCreateM3UFileMutation(addM3UFileRequest)
       .then(() => {
