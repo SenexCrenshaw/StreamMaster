4db656beab9db78c19bed5cb1614c533637f6d97
SenexCrenshaw <35600301+SenexCrenshaw@users.noreply.github.com>
2023-10-14 07:54:56 -0400

    change clock icon sev

--- streammasterwebui/components/buttons/ClockButton.tsx
+++ streammasterwebui/components/buttons/ClockButton.tsx
@@ -1,7 +1,7 @@
 import BaseButton, { type ChildButtonProps } from './BaseButton';
 
 const ClockButton: React.FC<ChildButtonProps> = ({ disabled = false, label, onClick, tooltip = 'Time Shift', iconFilled }) => {
-  return <BaseButton disabled={disabled} icon="pi-stopwatch" iconFilled={iconFilled} label={label} onClick={onClick} severity="danger" tooltip={tooltip} />;
+  return <BaseButton disabled={disabled} icon="pi-stopwatch" iconFilled={iconFilled} label={label} onClick={onClick} tooltip={tooltip} />;
 };
 
 export default ClockButton;
