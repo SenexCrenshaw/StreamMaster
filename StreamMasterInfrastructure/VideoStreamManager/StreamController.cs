4dd8b350209c861a2d00a1999369289e0f9c2081
SenexCrenshaw <35600301+SenexCrenshaw@users.noreply.github.com>
2023-10-15 12:20:00 -0400

    🔧 fix(StreamController.cs): fix logger variable name and add missing dependencies in constructor 🔧 fix(StreamController.cs): fix CheckProcessExists method to use ProcessId property instead of parameter ✨ feat(StreamController.cs): add logging and error handling for stream operations, implement GetProxy and StartVideoStreaming methods The logger variable in the StreamController class was incorrectly named, causing a compilation error. The variable has been renamed to _logger to match the ILogger<StreamController> type. Additionally, the constructor was missing some dependencies, including ILogger<StreamController> and IMemoryCache. These dependencies have been added to the constructor.
    The CheckProcessExists method was using a processId parameter instead of the ProcessId property. This has been fixed to use the correct property.
    
    The commit also introduces new features to the StreamController class. Logging and error handling have been added to the stream operations, including the GetProxy and StartVideoStreaming methods. The GetProxy method retrieves a proxy stream based on the stream URL and the specified streaming proxy type. The StartVideoStreaming method handles the actual streaming of the video, reading chunks of data from the stream and writing them to the circular buffer. Error handling and retry logic have been implemented to handle potential errors during the streaming process
    
    🔧 refactor(StreamManager.cs): refactor StreamManager constructor parameters to improve readability and maintainability
    🔧 refactor(StreamManager.cs): refactor GetOrCreateStreamController method to simplify logic and improve readability
    🔧 refactor(StreamManager.cs): remove unused private methods and unnecessary comments
    The constructor of the StreamManager class has been refactored to improve readability and maintainability. The constructor now takes in additional parameters for the StreamController logger and the StreamManager logger. This change allows for better separation of concerns and makes it easier to identify which logger is being used in each part of the code.
    
    The GetOrCreateStreamController method has been refactored to simplify the logic and improve readability. The method now creates a new StreamController object and adds it to the _streamControllers dictionary using the TryAdd method. This change eliminates the need for the CreateNewStreamController method and reduces the complexity of the code.
    
    Unused private methods and unnecessary comments have been removed to clean up the code and improve code quality.

--- StreamMasterInfrastructure/VideoStreamManager/StreamController.cs
+++ StreamMasterInfrastructure/VideoStreamManager/StreamController.cs
@@ -1,6 +1,13 @@
-﻿using StreamMasterApplication.Common.Interfaces;
+﻿using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Logging;
+
+using StreamMasterApplication.Common.Interfaces;
 using StreamMasterApplication.Common.Models;
 
+using StreamMasterDomain.Cache;
+using StreamMasterDomain.Common;
+using StreamMasterDomain.Dto;
+using StreamMasterDomain.Enums;
 using StreamMasterDomain.Events;
 
 using System.Diagnostics;
@@ -22,22 +29,182 @@ namespace StreamMasterInfrastructure.VideoStreamManager;
 
 public class StreamController : IDisposable, IStreamController
 {
-    public StreamController() { }
+    public IClientStreamerManager _clientManager { get; set; }
 
-    public StreamController(string streamUrl, IClientStreamerManager clientManager, ICircularRingBuffer buffer, Task streamingTask, int m3uFileId, int maxStreams, int processId, CancellationTokenSource cancellationTokenSource)
+    public StreamController() { }
+    private readonly ILogger<StreamController> _logger;
+    private readonly IMemoryCache _memoryCache;
+    public StreamController(
+        string streamUrl,
+        ChildVideoStreamDto childVideoStreamDto,
+        string videoStreamId,
+        string videoStreamName,
+        int rank,
+        ILogger<StreamController> logger,
+        IMemoryCache memoryCache,
+        ILogger<CircularRingBuffer> circularBufferLogger,
+        IClientStreamerManager clientStreamerManager)
     {
-        StreamUrl = streamUrl;
-        VideoStreamingCancellationToken = cancellationTokenSource;
-        StreamingTask = streamingTask;
-        RingBuffer = buffer;
-        M3UFileId = m3uFileId;
-        MaxStreams = maxStreams;
+        _logger = logger;
+        _clientManager = clientStreamerManager;
+        _memoryCache = memoryCache;
+        Setting setting = memoryCache.GetSetting();
+        ICircularRingBuffer buffer = new CircularRingBuffer(childVideoStreamDto, videoStreamId, videoStreamName, rank, setting.PreloadPercentage, setting.RingBufferSizeMB, circularBufferLogger);
+        CancellationTokenSource cancellationTokenSource = new();
+
+        // Assuming GetProxy is available in this class or you can move it here
+        (Stream? stream, int processId, ProxyStreamError? error) = GetProxy(streamUrl, cancellationTokenSource.Token).Result;  // Blocking wait with .Result
+
+        if (stream == null || error != null)
+        {
+            logger.LogError("Error in StreamController constructor for {StreamUrl}", streamUrl);
+            throw new InvalidOperationException($"Unable to create StreamController for {streamUrl} due to proxy error.");
+        }
+
         ProcessId = processId;
-        M3UStream = false;
-        _clientManager = clientManager;
+        StreamUrl = streamUrl;
+        RingBuffer = buffer;
+        VideoStreamingCancellationToken = cancellationTokenSource;
+
+
+        StartVideoStreaming(stream, buffer, cancellationTokenSource);
     }
 
-    public IClientStreamerManager _clientManager { get; set; }
+    private void LogErrorIfAny(Stream? stream, ProxyStreamError? error, string streamUrl)
+    {
+        if (stream == null || error != null)
+        {
+            _logger.LogError("Error getting proxy stream for {StreamUrl}: {Error?.Message}", streamUrl, error?.Message);
+        }
+    }
+
+
+    private async Task<(Stream? stream, int processId, ProxyStreamError? error)> GetProxy(string streamUrl, CancellationToken cancellationToken)
+    {
+        Stream? stream;
+        ProxyStreamError? error;
+        int processId;
+
+        Setting setting = _memoryCache.GetSetting();
+
+        if (setting.StreamingProxyType == StreamingProxyTypes.FFMpeg)
+        {
+            (stream, processId, error) = await StreamingProxies.GetFFMpegStream(streamUrl, _logger, setting);
+            LogErrorIfAny(stream, error, streamUrl);
+        }
+        else
+        {
+            (stream, processId, error) = await StreamingProxies.GetProxyStream(streamUrl, _logger, setting, cancellationToken);
+            LogErrorIfAny(stream, error, streamUrl);
+        }
+
+        return (stream, processId, error);
+    }
+
+    private async Task DelayWithCancellation(int milliseconds, CancellationToken cancellationToken)
+    {
+        try
+        {
+            await Task.Delay(milliseconds, cancellationToken);
+        }
+        catch (TaskCanceledException)
+        {
+            _logger.LogInformation("Task was cancelled");
+            throw;
+        }
+    }
+
+    private async Task LogRetryAndDelay(int retryCount, int maxRetries, int waitTime, CancellationToken token)
+    {
+        if (token.IsCancellationRequested)
+        {
+            _logger.LogInformation("Stream was cancelled for: {StreamUrl}", StreamUrl);
+        }
+
+        _logger.LogInformation("Stream received 0 bytes for stream: {StreamUrl} Retry {retryCount}/{maxRetries}",
+            StreamUrl,
+            retryCount,
+            maxRetries);
+
+        await DelayWithCancellation(waitTime, token);
+    }
+
+    private async Task StartVideoStreaming(Stream stream, ICircularRingBuffer buffer, CancellationTokenSource cancellationToken)
+    {
+        int chunkSize = 24 * 1024;
+
+        _logger.LogInformation("Starting video read streaming, chunk size is {ChunkSize}, for stream: {StreamUrl}", chunkSize, StreamUrl);
+
+        byte[] bufferChunk = new byte[chunkSize];
+
+        int maxRetries = 3; //setting.MaxConnectRetry > 0 ? setting.MaxConnectRetry : 3;
+        int waitTime = 50;// setting.MaxConnectRetryTimeMS > 0 ? setting.MaxConnectRetryTimeMS : 50;
+
+        using (stream)
+        {
+            int retryCount = 0;
+            while (!cancellationToken.IsCancellationRequested && retryCount < maxRetries)
+            {
+                try
+                {
+                    int bytesRead = await TryReadStream(bufferChunk, stream, cancellationToken.Token);
+                    if (bytesRead == -1)
+                    {
+                        break;
+                    }
+                    if (bytesRead == 0)
+                    {
+                        retryCount++;
+                        await LogRetryAndDelay(retryCount, maxRetries, waitTime, cancellationToken.Token);
+                    }
+                    else
+                    {
+                        buffer.WriteChunk(bufferChunk, bytesRead);
+                        retryCount = 0;
+                        //LogBufferHealth(buffer);
+                    }
+                }
+                catch (TaskCanceledException ex)
+                {
+                    _logger.LogError(ex, "Stream cancelled for: {StreamUrl}", StreamUrl);
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, "Stream error for: {StreamUrl}", StreamUrl);
+                    break;
+                }
+            }
+        }
+
+        _logger.LogInformation("Stream stopped for: {StreamUrl}", StreamUrl);
+        if (!cancellationToken.IsCancellationRequested)
+        {
+            cancellationToken.Cancel();
+        }
+    }
+
+    private async Task<int> TryReadStream(byte[] bufferChunk, Stream stream, CancellationToken cancellationToken)
+    {
+        try
+        {
+            if (!stream.CanRead || cancellationToken.IsCancellationRequested)
+            {
+                _logger.LogWarning("Stream is not readable or cancelled");
+                return -1;
+            }
+
+            return await stream.ReadAsync(bufferChunk, cancellationToken);
+        }
+        catch (TaskCanceledException)
+        {
+            return -1;
+        }
+        catch (Exception)
+        {
+            throw;
+        }
+    }
+
 
     /// <summary>
     /// Raised when an error occurs during stream operations.
@@ -70,7 +237,7 @@ public class StreamController : IDisposable, IStreamController
 
     public int ProcessId { get; set; } = -1;
 
-    public ICircularRingBuffer RingBuffer { get; private set; }
+    public ICircularRingBuffer RingBuffer { get; }
 
     public Task StreamingTask { get; set; }
     public string StreamUrl { get; set; }
@@ -106,7 +273,7 @@ public class StreamController : IDisposable, IStreamController
         }
         catch (Exception ex)
         {
-            // logger.LogError(ex, "Error registering stream configuration for client {ClientId}.", streamerConfiguration.ClientId);
+            _logger.LogError(ex, "Error registering stream configuration for client {ClientId}.", streamerConfiguration.ClientId);
             StreamFailed?.Invoke(this, new StreamFailedEventArgs($"Failed to register client {streamerConfiguration.ClientId}."));
         }
 
@@ -129,7 +296,7 @@ public class StreamController : IDisposable, IStreamController
         {
             try
             {
-                string? procName = CheckProcessExists(ProcessId);
+                string? procName = CheckProcessExists();
                 if (procName != null)
                 {
                     Process process = Process.GetProcessById(ProcessId);
@@ -138,7 +305,7 @@ public class StreamController : IDisposable, IStreamController
             }
             catch (Exception ex)
             {
-                // logger.LogError(ex, "Error killing process {ProcessId}.", ProcessId);
+                _logger.LogError(ex, "Error killing process {ProcessId}.", ProcessId);
             }
         }
         OnStreamControllerStopped();
@@ -157,16 +324,16 @@ public class StreamController : IDisposable, IStreamController
         }
         catch (Exception ex)
         {
-            // logger.LogError(ex, "Error unregistering stream configuration for client {ClientId}.", streamerConfiguration.ClientId);
+            _logger.LogError(ex, "Error unregistering stream configuration for client {ClientId}.", streamerConfiguration.ClientId);
             return false;
         }
     }
 
-    private string? CheckProcessExists(int processId)
+    private string? CheckProcessExists()
     {
         try
         {
-            Process process = Process.GetProcessById(processId);
+            Process process = Process.GetProcessById(ProcessId);
             // logger.LogInformation($"Process with ID {processId} exists. Name: {process.ProcessName}");
             return process.ProcessName;
         }
--- StreamMasterInfrastructure/VideoStreamManager/StreamManager.cs
+++ StreamMasterInfrastructure/VideoStreamManager/StreamManager.cs
@@ -4,16 +4,14 @@ using Microsoft.Extensions.Logging;
 using StreamMasterApplication.Common.Interfaces;
 using StreamMasterApplication.Common.Models;
 
-using StreamMasterDomain.Cache;
 using StreamMasterDomain.Common;
 using StreamMasterDomain.Dto;
-using StreamMasterDomain.Enums;
 
 using System.Collections.Concurrent;
 
 namespace StreamMasterInfrastructure.VideoStreamManager;
 
-public class StreamManager(ILogger<CircularRingBuffer> circularBufferLogger, IClientStreamerManager clientStreamerManager, ILogger<StreamManager> logger, IMemoryCache memoryCache) : IStreamManager
+public class StreamManager(ILogger<CircularRingBuffer> circularBufferLogger, IClientStreamerManager clientStreamerManager, ILogger<StreamController> streamControllerlogger, ILogger<StreamManager> logger, IMemoryCache memoryCache) : IStreamManager
 {
 
     private readonly ConcurrentDictionary<string, IStreamController> _streamControllers = new();
@@ -29,48 +27,16 @@ public class StreamManager(ILogger<CircularRingBuffer> circularBufferLogger, ICl
         // Clear the dictionary
         _streamControllers.Clear();
     }
-    private async Task<IStreamController> CreateNewStreamController(string streamUrl, ChildVideoStreamDto childVideoStreamDto, string videoStreamId, string videoStreamName, int rank)
-    {
-        Setting setting = memoryCache.GetSetting();
-
-        ICircularRingBuffer buffer = new CircularRingBuffer(childVideoStreamDto, videoStreamId, videoStreamName, rank, setting.PreloadPercentage, setting.RingBufferSizeMB, circularBufferLogger);
-        CancellationTokenSource cancellationTokenSource = new();
-
-        (Stream? stream, int processId, ProxyStreamError? error) = await GetProxy(streamUrl, cancellationTokenSource.Token);
-
-        if (stream == null || error != null)
-        {
-            logger.LogError("Error in CreateNewStreamController for {StreamUrl}", streamUrl);
-            throw new InvalidOperationException($"Unable to create StreamController for {streamUrl} due to proxy error.");
-        }
-
-        Task streamingTask = StartVideoStreaming(stream, streamUrl, buffer, cancellationTokenSource);
-
-        StreamController streamController = new(streamUrl, clientStreamerManager, buffer, streamingTask, childVideoStreamDto.M3UFileId, childVideoStreamDto.MaxStreams, processId, cancellationTokenSource);
-
-        logger.LogInformation("Created new StreamController for: {StreamUrl}", streamUrl);
-
-        return streamController;
-    }
 
 
     public async Task<IStreamController?> GetOrCreateStreamController(ChildVideoStreamDto childVideoStreamDto, string videoStreamId, string videoStreamName, int rank)
     {
         string streamUrl = childVideoStreamDto.User_Url;
 
-
         if (!_streamControllers.TryGetValue(streamUrl, out IStreamController streamController))
         {
-            // If the stream controller doesn't exist, create a new one
-            IStreamController newStreamController = await CreateNewStreamController(streamUrl, childVideoStreamDto, videoStreamId, videoStreamName, rank);
-
-            // Add or get existing StreamController
-            streamController = _streamControllers.AddOrUpdate(streamUrl, newStreamController, (key, existing) => existing);
-
-            if (streamController.StreamUrl == streamUrl)
-            {
-                logger.LogInformation("Created buffer for stream: {StreamUrl}", streamUrl);
-            }
+            streamController = new StreamController(streamUrl, childVideoStreamDto, videoStreamId, videoStreamName, rank, streamControllerlogger, memoryCache, circularBufferLogger, clientStreamerManager);
+            _streamControllers.TryAdd(streamUrl, streamController);
         }
         else
         {
@@ -79,6 +45,7 @@ public class StreamManager(ILogger<CircularRingBuffer> circularBufferLogger, ICl
 
         return streamController;
     }
+
 
 
     public SingleStreamStatisticsResult GetSingleStreamStatisticsResult(string streamUrl)
@@ -114,50 +81,6 @@ public class StreamManager(ILogger<CircularRingBuffer> circularBufferLogger, ICl
         logger.LogWarning("Failed to remove stream information for {StreamUrl}", streamUrl);
         return null;
     }
-    private async Task DelayWithCancellation(int milliseconds, CancellationToken cancellationToken)
-    {
-        try
-        {
-            await Task.Delay(milliseconds, cancellationToken);
-        }
-        catch (TaskCanceledException)
-        {
-            logger.LogInformation("Task was cancelled");
-            throw;
-        }
-    }
-    private async Task<(Stream? stream, int processId, ProxyStreamError? error)> GetProxy(string streamUrl, CancellationToken cancellationToken)
-    {
-        Stream? stream;
-        ProxyStreamError? error;
-        int processId;
-
-        Setting setting = memoryCache.GetSetting();
-
-        if (setting.StreamingProxyType == StreamingProxyTypes.FFMpeg)
-        {
-            (stream, processId, error) = await StreamingProxies.GetFFMpegStream(streamUrl, logger, setting);
-            LogErrorIfAny(stream, error, streamUrl);
-        }
-        else
-        {
-            (stream, processId, error) = await StreamingProxies.GetProxyStream(streamUrl, logger, setting, cancellationToken);
-            LogErrorIfAny(stream, error, streamUrl);
-        }
-
-        return (stream, processId, error);
-    }
-
-    private void LogBufferHealth(ICircularRingBuffer buffer)
-    {
-        // Calculate buffer utilization percentage
-        float bufferUtilization = buffer.GetBufferUtilization();
-        if (bufferUtilization < 95.0)
-        {
-            // Log the buffer health information
-            logger.LogWarning("Buffer health below 95% - Capacity: {BufferCapacity}, Utilization: {BufferUtilization}%", buffer.BufferSize, bufferUtilization);
-        }
-    }
 
     private void LogErrorIfAny(Stream? stream, ProxyStreamError? error, string streamUrl)
     {
@@ -167,94 +90,4 @@ public class StreamManager(ILogger<CircularRingBuffer> circularBufferLogger, ICl
         }
     }
 
-    private async Task LogRetryAndDelay(int retryCount, int maxRetries, int waitTime, string streamUrl, CancellationToken token)
-    {
-        if (token.IsCancellationRequested)
-        {
-            logger.LogInformation("Stream was cancelled for: {StreamUrl}", streamUrl);
-        }
-
-        logger.LogInformation("Stream received 0 bytes for stream: {StreamUrl} Retry {retryCount}/{maxRetries}",
-            streamUrl,
-            retryCount,
-            maxRetries);
-
-        await DelayWithCancellation(waitTime, token);
-    }
-
-    private async Task StartVideoStreaming(Stream stream, string streamUrl, ICircularRingBuffer buffer, CancellationTokenSource cancellationToken)
-    {
-        int chunkSize = 24 * 1024;
-
-        logger.LogInformation("Starting video read streaming, chunk size is {ChunkSize}, for stream: {StreamUrl}", chunkSize, streamUrl);
-
-        byte[] bufferChunk = new byte[chunkSize];
-
-        int maxRetries = 3; //setting.MaxConnectRetry > 0 ? setting.MaxConnectRetry : 3;
-        int waitTime = 50;// setting.MaxConnectRetryTimeMS > 0 ? setting.MaxConnectRetryTimeMS : 50;
-
-        using (stream)
-        {
-            int retryCount = 0;
-            while (!cancellationToken.IsCancellationRequested && retryCount < maxRetries)
-            {
-                try
-                {
-                    int bytesRead = await TryReadStream(bufferChunk, stream, cancellationToken.Token);
-                    if (bytesRead == -1)
-                    {
-                        break;
-                    }
-                    if (bytesRead == 0)
-                    {
-                        retryCount++;
-                        await LogRetryAndDelay(retryCount, maxRetries, waitTime, streamUrl, cancellationToken.Token);
-                    }
-                    else
-                    {
-                        buffer.WriteChunk(bufferChunk, bytesRead);
-                        retryCount = 0;
-                        //LogBufferHealth(buffer);
-                    }
-                }
-                catch (TaskCanceledException ex)
-                {
-                    logger.LogError(ex, "Stream cancelled for: {StreamUrl}", streamUrl);
-                }
-                catch (Exception ex)
-                {
-                    logger.LogError(ex, "Stream error for: {StreamUrl}", streamUrl);
-                    break;
-                }
-            }
-        }
-
-        logger.LogInformation("Stream stopped for: {StreamUrl}", streamUrl);
-        if (!cancellationToken.IsCancellationRequested)
-        {
-            cancellationToken.Cancel();
-        }
-    }
-
-    private async Task<int> TryReadStream(byte[] bufferChunk, Stream stream, CancellationToken cancellationToken)
-    {
-        try
-        {
-            if (!stream.CanRead || cancellationToken.IsCancellationRequested)
-            {
-                logger.LogWarning("Stream is not readable or cancelled");
-                return -1;
-            }
-
-            return await stream.ReadAsync(bufferChunk, cancellationToken);
-        }
-        catch (TaskCanceledException)
-        {
-            return -1;
-        }
-        catch (Exception)
-        {
-            throw;
-        }
-    }
 }
