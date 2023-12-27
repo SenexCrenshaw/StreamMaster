b7b0928d2eee4a6401c2028d8a1e30381c6c7660
SenexCrenshaw <35600301+SenexCrenshaw@users.noreply.github.com>
2023-12-27 09:43:31 -0500

    Added in m3u generator for testing

---
+++ M3UGenerator/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace M3UGenerator
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            List<string> m3u = BuildM3ULineForVideoStream(200000);
+            File.WriteAllLines("test.m3u", m3u);
+        }
+
+        private static List<string> BuildM3ULineForVideoStream(int m3uCount = 100)
+        {
+            List<string> lines = [];
+            lines.Add("#EXTM3U");
+            for (int i = 0; i < m3uCount; i++)
+            {
+                string id = Guid.NewGuid().ToString();
+                lines.Add($"#EXTINF:-1 tvg-id=\"Channel_{i}\" tvg-name=\"Channel {i}\" tvg-logo=\"https://logo{i}.png\" group-title =\"TEST CHANNEL GROUP\", Channel {i}");
+                lines.Add($"http://channelfake.test/live/{id}.ts");
+            }
+            return lines;// string.Join("\r\n", lines.ToArray());
+        }
+
+
+    }
+
+}
