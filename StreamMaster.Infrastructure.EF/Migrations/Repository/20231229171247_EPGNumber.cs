6be7b3704720d7af4853d6b190994330081df0ce
SenexCrenshaw <35600301+SenexCrenshaw@users.noreply.github.com>
2023-12-30 08:10:05 -0500

    migration fix

--- StreamMaster.Infrastructure.EF/Migrations/Repository/20231229171247_EPGNumber.cs
+++ StreamMaster.Infrastructure.EF/Migrations/Repository/20231229171247_EPGNumber.cs
@@ -10,21 +10,58 @@ namespace StreamMaster.Infrastructure.EF.Migrations.Repository
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.RenameColumn(
-                name: "EPGRank",
-                table: "EPGFiles",
-                newName: "EPGNumber");
+            try
+            {
+                var dropColumnScript = @"
+        IF EXISTS(SELECT * FROM INFORMATION_SCHEMA.COLUMNS 
+                  WHERE TABLE_NAME = 'EPGFiles' AND COLUMN_NAME = 'EPGRank')
+        BEGIN
+            ALTER TABLE EPGFiles DROP COLUMN ""EPGRank"";
+        END";
+                migrationBuilder.Sql(dropColumnScript);
 
+                var addColumnScript = @"
+        IF NOT EXISTS(SELECT * FROM INFORMATION_SCHEMA.COLUMNS 
+                  WHERE TABLE_NAME = 'EPGFiles' AND COLUMN_NAME = 'EPGNumber')
+        BEGIN
+            ALTER TABLE EPGFiles ADD COLUMN ""EPGNumber"" INTEGER NOT NULL DEFAULT 0;
+        END";
+                migrationBuilder.Sql(addColumnScript);
+
+                //migrationBuilder.AddColumn<int>(
+                //    name: "EPGNumber",
+                //    table: "EPGFiles",
+                //    type: "INTEGER",
+                //    nullable: false,
+                //    defaultValue: 0
+                //);
+                //migrationBuilder.RenameColumn(
+                //    name: "EPGRank",
+                //    table: "EPGFiles",
+                //    newName: "EPGNumber");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
             migrationBuilder.Sql("UPDATE EPGFiles SET EPGNumber = Id where EPGNumber='0'");
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.RenameColumn(
+            try
+            {
+                migrationBuilder.RenameColumn(
                 name: "EPGNumber",
                 table: "EPGFiles",
                 newName: "EPGRank");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
+
 }
