7a81301d411c863350b6c282e787ba61c458af0a
SenexCrenshaw <35600301+SenexCrenshaw@users.noreply.github.com>
2024-01-30 09:33:33 -0500

    fix(packaging): Updated versioning

--- StreamMaster.API/AssemblyInfo.cs
+++ StreamMaster.API/AssemblyInfo.cs
@@ -1,7 +1,5 @@
-﻿
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.4.9.0")]
-[assembly: AssemblyFileVersion("0.4.9.0")]
-[assembly: AssemblyInformationalVersion("0.4.9-versioning.2+Branch.versioning.Sha.20b37a4dc75ef87d51d9f92cece2ff764ce59788")]
-
+[assembly: AssemblyVersion("0.5.0-versioning.5")]
+[assembly: AssemblyFileVersion("0.5.0-versioning.5")]
+[assembly: AssemblyInformationalVersion("0.5.0-versioning.5-versioning.2+Branch.versioning.Sha.20b37a4dc75ef87d51d9f92cece2ff764ce59788")]
