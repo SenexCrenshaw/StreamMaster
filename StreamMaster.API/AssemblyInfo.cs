65e80a92a5578b7ce09d1d2b49462f6a3032102b
SenexCrenshaw <35600301+SenexCrenshaw@users.noreply.github.com>
2024-01-30 09:35:08 -0500

    chore: update AssemblyInfo.cs to version 0.5.0-versioning.6

--- StreamMaster.API/AssemblyInfo.cs
+++ StreamMaster.API/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.5.0-versioning.5")]
-[assembly: AssemblyFileVersion("0.5.0-versioning.5")]
-[assembly: AssemblyInformationalVersion("0.5.0-versioning.5-versioning.2+Branch.versioning.Sha.20b37a4dc75ef87d51d9f92cece2ff764ce59788")]
+[assembly: AssemblyVersion("0.5.0-versioning.6")]
+[assembly: AssemblyFileVersion("0.5.0-versioning.6")]
+[assembly: AssemblyInformationalVersion("0.5.0-versioning.6-versioning.2+Branch.versioning.Sha.20b37a4dc75ef87d51d9f92cece2ff764ce59788")]
