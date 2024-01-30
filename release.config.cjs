8528a298fa04d8db7414e5b74ee11e0f0da3d4b6
SenexCrenshaw <35600301+SenexCrenshaw@users.noreply.github.com>
2024-01-30 10:11:16 -0500

    chore: update release configuration files

---
+++ release.config.cjs
@@ -0,0 +1,58 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+
+module.exports = {
+  branches: [
+    // "+([0-9])?(.{+([0-9]),x}).x",
+    "main",
+    // {
+    //   name: "beta",
+    //   prerelease: true
+    // },
+    // {
+    //   name: "alpha",
+    //   prerelease: true
+    // },
+    {
+      name: "!main",
+      prerelease: true
+    }
+  ],
+  ci: false,
+  debug: true,
+  dryRun: false,
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/changelog",
+      {
+        changelogFile: "CHANGELOG.md"
+      }
+    ],
+    [
+      "@semantic-release/exec",
+      {
+        verifyConditionsCmd: ":",
+        publishCmd: [
+          "node updateAssemblyInfo.js ${nextRelease.version}",
+          "git add ./StreamMaster.API/AssemblyInfo.cs",
+          'git diff-index --quiet HEAD || git commit -m "chore: update AssemblyInfo.cs to version ${nextRelease.version}"'
+        ].join(" && ")
+      }
+    ],
+    [
+      "@semantic-release/git",
+      {
+        assets: ["CHANGELOG.md"]
+      }
+    ],
+    [
+      "@semantic-release/github",
+      {
+        assets: ["CHANGELOG.md"]
+      }
+    ]
+  ]
+};
