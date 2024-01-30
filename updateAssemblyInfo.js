50cf0d0e78f8b886fc707f885f37dcc94d96ee19
SenexCrenshaw <35600301+SenexCrenshaw@users.noreply.github.com>
2024-01-30 09:19:32 -0500

    feat(packaging): Add exec

---
+++ updateAssemblyInfo.js
@@ -0,0 +1,23 @@
+const fs = require("fs").promises;
+
+const version = process.argv[2]; // version from semantic-release
+const filePath = "./AssemblyInfo.cs";
+const content = `
+using System.Reflection;
+
+[assembly: AssemblyVersion("${version}")]
+[assembly: AssemblyFileVersion("${version}")]
+[assembly: AssemblyInformationalVersion("${version}-versioning.2+Branch.versioning.Sha.20b37a4dc75ef87d51d9f92cece2ff764ce59788")]
+`;
+
+async function createOrUpdateAssemblyInfo() {
+  try {
+    // Write the content to AssemblyInfo.cs
+    await fs.writeFile(filePath, content.trim(), "utf8");
+    console.log("AssemblyInfo.cs has been created/updated successfully.");
+  } catch (error) {
+    console.error("Error creating/updating AssemblyInfo.cs:", error);
+  }
+}
+
+createOrUpdateAssemblyInfo();
