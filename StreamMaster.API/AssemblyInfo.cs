c8547b2faface6ac15e46a8441491d611f438808
SenexCrenshaw <35600301+SenexCrenshaw@users.noreply.github.com>
2024-01-30 07:22:22 -0500

    test

--- StreamMaster.API/AssemblyInfo.cs
+++ StreamMaster.API/AssemblyInfo.cs
@@ -3,5 +3,5 @@ using System.Reflection;
 
 [assembly: AssemblyVersion("0.4.9.0")]
 [assembly: AssemblyFileVersion("0.4.9.0")]
-[assembly: AssemblyInformationalVersion("0.4.9-versioning.1+Branch.versioning.Sha.c2554079bc289300d31782afa0cc74098c79d381")]
+[assembly: AssemblyInformationalVersion("0.4.9-versioning.1+Branch.versioning.Sha.7fe9b35f37930c228f5544e78ecfe89b7a633025")]
 
