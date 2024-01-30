f1d0cce891972f438fe9545a9ccd0c00a219527b
SenexCrenshaw <35600301+SenexCrenshaw@users.noreply.github.com>
2024-01-30 13:14:09 -0500

    update: Add new feature for user login
    This commit adds a new feature for user, allowing users to securely log in to the application. This will trigger a release bumping a MINOR version.

--- release.config.cjs
+++ release.config.cjs
@@ -23,7 +23,21 @@ module.exports = {
   debug: false,
   dryRun: false,
   plugins: [
-    "@semantic-release/commit-analyzer",
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        preset: "angular",
+        releaseRules: [
+          { type: "docs", scope: "README", release: "patch" },
+          { type: "refactor", release: "patch" },
+          { type: "style", release: "patch" },
+          { type: "add", release: "patch" }
+        ],
+        parserOpts: {
+          noteKeywords: ["BREAKING CHANGE", "BREAKING CHANGES"]
+        }
+      }
+    ],
     "@semantic-release/release-notes-generator",
     [
       "@semantic-release/changelog",
