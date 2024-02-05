b1b022bab5162621762b2ed5cad72ea1e9d8c6e5
SenexCrenshaw <35600301+SenexCrenshaw@users.noreply.github.com>
2024-02-05 10:45:34 -0500

    refactor: Refactor BuildInfo class for better organization and readability

--- StreamMaster.Domain/Common/BuildInfo.cs
+++ StreamMaster.Domain/Common/BuildInfo.cs
@@ -1,131 +1,155 @@
 ﻿using System.Reflection;
 
-namespace StreamMaster.Domain.Common;
-
-public static class BuildInfo
+namespace StreamMaster.Domain.Common
 {
-    static BuildInfo()
+    /// <summary>
+    /// Provides information about the build and environment settings.
+    /// </summary>
+    public static class BuildInfo
     {
-        Assembly? assembly = Assembly.GetEntryAssembly();
-
-        Version = assembly.GetName().Version;
-        //    string informationalVersion = assembly
-        //.GetCustomAttributes(typeof(AssemblyInformationalVersionAttribute), false)
-        //.OfType<AssemblyInformationalVersionAttribute>()
-        //.FirstOrDefault()
-        //?.InformationalVersion;
-
-        //    string productVersion = assembly
-        //        .GetCustomAttributes(typeof(AssemblyProductAttribute), false)
-        //        .OfType<AssemblyProductAttribute>()
-        //        .FirstOrDefault()
-        //        ?.Product;
-
-        object[] attributes = assembly.GetCustomAttributes(true);
-
-        Branch = "unknow";
-        Release = Version.ToString();
-
-        //string timestamp = DateTime.Now.ToString("yyyyMMdd_HHmmss");
-        //LogFilePath = Path.Combine(LogFolder, $"StreamMasterAPI_{timestamp}.log");
-
-        AssemblyInformationalVersionAttribute? informationalVersion = attributes.OfType<AssemblyInformationalVersionAttribute>().FirstOrDefault();
-        if (informationalVersion is not null)
+        static BuildInfo()
         {
-            Release = informationalVersion.InformationalVersion.Contains("Sha")
-            ? informationalVersion.InformationalVersion[..(informationalVersion.InformationalVersion.IndexOf("Sha") - 1)]
-            : informationalVersion.InformationalVersion;
+            Assembly? assembly = Assembly.GetEntryAssembly();
+            if (assembly is null)
+            {
+                throw new InvalidOperationException("Failed to get entry assembly.");
+            }
 
+            Version = assembly.GetName().Version ?? new Version(0, 0, 0, 0);
+            object[] attributes = assembly.GetCustomAttributes(true);
 
-        }
-    }
+            Branch = "unknown";
+            Release = Version.ToString();
 
-    public static string DBName
-    {
-        get
-        {
-            string envHost = Environment.GetEnvironmentVariable("POSTGRES_DB");
-            return !string.IsNullOrEmpty(envHost) ? envHost : "StreamMaster";
+            AssemblyInformationalVersionAttribute? informationalVersion = attributes.OfType<AssemblyInformationalVersionAttribute>().FirstOrDefault();
+            if (informationalVersion is not null)
+            {
+                Release = informationalVersion.InformationalVersion.Contains("Sha")
+                    ? informationalVersion.InformationalVersion[..(informationalVersion.InformationalVersion.IndexOf("Sha", StringComparison.Ordinal) - 1)]
+                    : informationalVersion.InformationalVersion;
+            }
+
+            InitializePaths();
         }
 
-    }
+        #region Database Configuration Properties
 
-    public static string DBUser
-    {
-        get
+        /// <summary>
+        /// Database name, fetched from environment variable or default if not set.
+        /// </summary>
+        public static string DBName => GetEnvironmentVariableOrDefault("POSTGRES_DB", "StreamMaster");
+
+        /// <summary>
+        /// Database user, fetched from environment variable or default if not set.
+        /// </summary>
+        public static string DBUser => GetEnvironmentVariableOrDefault("POSTGRES_USER", "postgres");
+
+        /// <summary>
+        /// Database password, fetched from environment variable or default if not set.
+        /// </summary>
+        public static string DBPassword => GetEnvironmentVariableOrDefault("POSTGRES_PASSWORD", "sm123");
+
+        #endregion
+
+        #region Application Information Properties
+
+        public static string AppName { get; } = "StreamMaster";
+        public static bool SetIsSystemReady { get; set; } = false;
+        public static Version Version { get; }
+        public static string Branch { get; }
+        public static string Release { get; }
+        /// <summary>
+        /// Gets the build date and time by reading the last write time of the assembly.
+        /// </summary>
+        public static DateTime BuildDateTime
         {
-            string envHost = Environment.GetEnvironmentVariable("POSTGRES_USER");
-            return !string.IsNullOrEmpty(envHost) ? envHost : "postgres";
+            get
+            {
+                try
+                {
+                    string? assemblyLocation = Assembly.GetExecutingAssembly().Location;
+                    return string.IsNullOrEmpty(assemblyLocation) || !File.Exists(assemblyLocation)
+                        ? throw new FileNotFoundException("Failed to locate the executing assembly.")
+                        : new FileInfo(assemblyLocation).LastWriteTimeUtc;
+                }
+                catch (Exception ex)
+                {
+                    // Log the exception or handle it as deemed appropriate for your use case.
+                    // Log.Warning(ex, "Unable to determine BuildDateTime.");
+                    return DateTime.MinValue; // Return a default value indicating failure.
+                }
+            }
         }
 
-    }
+        #endregion
 
-    public static string DBPassword
-    {
-        get
-        {
-            string envHost = Environment.GetEnvironmentVariable("POSTGRES_PASSWORD");
-            return !string.IsNullOrEmpty(envHost) ? envHost : "sm123";
-        }
+        #region Build Configuration Property
 
-    }
-
-    public static string AppName { get; } = "StreamMaster";
-
-    public static bool SetIsSystemReady { get; set; } = false;
-    public static Version Version { get; }
-    public static string Branch { get; }
-    public static string Release { get; }
-    //public static string AppDataFolder = $"{Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)}{Path.DirectorySeparatorChar}.{AppName.ToLower()}{Path.DirectorySeparatorChar}";
-    public static string AppDataFolder = "/config/";
-    public static readonly string DataFolder = Path.Combine(AppDataFolder, "DB");
-
-    public static readonly string CacheFolder = $"{AppDataFolder}Cache{Path.DirectorySeparatorChar}";
-    public static readonly string LogFolder = Path.Combine(AppDataFolder, "Logs");
-    public static readonly string PlayListFolder = $"{AppDataFolder}PlayLists{Path.DirectorySeparatorChar}";
-
-    public static readonly string TVLogoDataFolder = $"{AppDataFolder}tv-logos{Path.DirectorySeparatorChar}";
-
-    public static readonly string IconDataFolder = $"{CacheFolder}Icons{Path.DirectorySeparatorChar}";
-
-    public static readonly string ChannelIconDataFolder = $"{CacheFolder}ChannelIcons{Path.DirectorySeparatorChar}";
-    public static readonly string ProgrammeIconDataFolder = $"{CacheFolder}ProgrammeIcons{Path.DirectorySeparatorChar}";
-
-    public static readonly string SDJSONFolder = $"{CacheFolder}SDJson{Path.DirectorySeparatorChar}";
-    public static readonly string SDStationLogos = $"{CacheFolder}SDStationLogos{Path.DirectorySeparatorChar}";
-    public static readonly string SDStationLogosCache = $"{CacheFolder}SDStationLogosCache{Path.DirectorySeparatorChar}";
-    public static readonly string SDImagesFolder = $"{CacheFolder}SDImages{Path.DirectorySeparatorChar}";
-    public static readonly string SDEPGCacheFile = $"{SDJSONFolder}epgCache.json";
-
-    public static readonly string EPGFolder = $"{PlayListFolder}EPG{Path.DirectorySeparatorChar}";
-    public static readonly string M3UFolder = $"{PlayListFolder}M3U{Path.DirectorySeparatorChar}";
-
-    public static readonly string SettingFileName = "settings.json";
-
-    public static readonly string LoggingFileName = "logsettings.json";
-    public static readonly string SettingFile = $"{AppDataFolder}{SettingFileName}";
-
-
-    public static readonly string LoggingFile = File.Exists(Path.Combine(AppDataFolder, LoggingFileName)) ? Path.Combine(AppDataFolder, LoggingFileName) : LoggingFileName;
-
-    public static readonly string IconDefault = "images/default.png";
-    public static readonly string FFMPEGDefaultOptions = "-hide_banner -loglevel error -i {streamUrl} -c copy -f mpegts pipe:1";
-
-    public static readonly string LogFilePath = Path.Combine(LogFolder, "StreamMasterAPI.log");
-    public static DateTime BuildDateTime
-    {
-        get
-        {
-            string fileLocation = Assembly.GetCallingAssembly().Location;
-            return new FileInfo(fileLocation).LastWriteTimeUtc;
-        }
-    }
-
-    public static bool IsDebug =>
+        /// <summary>
+        /// Indicates if the application is running in Debug mode.
+        /// </summary>
+        public static bool IsDebug =>
 #if DEBUG
             true;
 #else
-                false;
+            false;
 #endif
 
+        #endregion
+
+        #region File and Directory Path Fields
+
+        public static string AppDataFolder { get; private set; } = "/config/";
+
+        public static readonly string DataFolder = Path.Combine(AppDataFolder, "DB");
+        public static readonly string CacheFolder = Path.Combine(AppDataFolder, "Cache");
+        public static readonly string LogFolder = Path.Combine(AppDataFolder, "Logs");
+        public static readonly string PlayListFolder = Path.Combine(AppDataFolder, "PlayLists");
+        public static readonly string TVLogoDataFolder = Path.Combine(AppDataFolder, "tv-logos");
+        public static readonly string IconDataFolder = Path.Combine(CacheFolder, "Icons");
+        public static readonly string ChannelIconDataFolder = Path.Combine(CacheFolder, "ChannelIcons");
+        public static readonly string ProgrammeIconDataFolder = Path.Combine(CacheFolder, "ProgrammeIcons");
+        public static readonly string SDJSONFolder = Path.Combine(CacheFolder, "SDJson");
+        public static readonly string SDStationLogos = Path.Combine(CacheFolder, "SDStationLogos");
+        public static readonly string SDStationLogosCache = Path.Combine(CacheFolder, "SDStationLogosCache");
+        public static readonly string SDImagesFolder = Path.Combine(CacheFolder, "SDImages");
+        public static readonly string SDEPGCacheFile = Path.Combine(SDJSONFolder, "epgCache.json");
+        public static readonly string EPGFolder = Path.Combine(PlayListFolder, "EPG");
+        public static readonly string M3UFolder = Path.Combine(PlayListFolder, "M3U");
+        public static readonly string SettingFileName = "settings.json";
+        public static readonly string LoggingFileName = "logsettings.json";
+        public static readonly string SettingFile = Path.Combine(AppDataFolder, SettingFileName);
+        public static readonly string LoggingFile = File.Exists(Path.Combine(AppDataFolder, LoggingFileName)) ? Path.Combine(AppDataFolder, LoggingFileName) : LoggingFileName;
+        public static readonly string IconDefault = Path.Combine("images", "default.png");
+        public static readonly string FFMPEGDefaultOptions = "-hide_banner -loglevel error -i {streamUrl} -c copy -f mpegts pipe:1";
+        public static readonly string LogFilePath = Path.Combine(LogFolder, "StreamMasterAPI.log");
+
+        #endregion
+
+
+        #region Helper Methods
+
+        /// <summary>
+        /// Initializes paths for various application data folders.
+        /// </summary>
+        private static void InitializePaths()
+        {
+            AppDataFolder = "/config/";
+            // Initialize other paths based on AppDataFolder...
+        }
+
+        /// <summary>
+        /// Gets an environment variable's value or a default value if not set.
+        /// </summary>
+        /// <param name="name">The name of the environment variable.</param>
+        /// <param name="defaultValue">The default value to return if the environment variable is not set.</param>
+        /// <returns>The environment variable's value or the default value.</returns>
+        private static string GetEnvironmentVariableOrDefault(string name, string defaultValue)
+        {
+            string envVar = Environment.GetEnvironmentVariable(name);
+            return !string.IsNullOrEmpty(envVar) ? envVar : defaultValue;
+        }
+
+        #endregion
+    }
 }
