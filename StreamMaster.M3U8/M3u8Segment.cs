1e9607497b0485291340a9489d6bb03e5ae5ef51
SenexCrenshaw <35600301+SenexCrenshaw@users.noreply.github.com>
2024-02-24 08:29:43 -0500

    fix: remove old code

--- StreamMaster.M3U8/M3u8Segment.cs
+++
@@ -1,8 +0,0 @@
-﻿public class M3u8Segment
-{
-    public long ByterangeLength { get; set; } = -1;
-    public long ByterangeStart { get; set; }
-    public double Duration { get; set; }
-    public int MediaSequence { get; set; }
-    public string Uri { get; set; }
-}
