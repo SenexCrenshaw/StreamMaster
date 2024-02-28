f104377fcebcc2f882a7f153622fd95bae1fbec9
SenexCrenshaw <35600301+SenexCrenshaw@users.noreply.github.com>
2024-02-28 13:40:07 -0500

    fix: GetIsSystemReady

--- streammasterwebui/lib/smAPI/Settings/SettingsGetAPI.tsx
+++ streammasterwebui/lib/smAPI/Settings/SettingsGetAPI.tsx
@@ -1,16 +1,12 @@
-/* eslint unused-imports/no-unused-imports-ts: off */
-/* eslint @typescript-eslint/no-unused-vars: off */
-import { StringArgument } from '@lib/common/dataTypes';
 import type * as iptv from '@lib/iptvApi';
 import { invokeHubConnection } from '@lib/signalr/signalr';
 
-export const GetIsSystemReady = async (): Promise<void | null> => {
-  await invokeHubConnection<void>('GetIsSystemReady');
+export const GetIsSystemReady = async (): Promise<boolean | null> => {
+  return invokeHubConnection<boolean>('GetIsSystemReady');
 };
-export const GetSetting = async (argument: iptv.SettingDto): Promise<iptv.SettingDto | null> =>
-  invokeHubConnection<iptv.SettingDto>('GetSetting', argument);
-export const GetSystemStatus = async (argument: iptv.SDSystemStatus): Promise<iptv.SDSystemStatus | null> =>
-  invokeHubConnection<iptv.SDSystemStatus>('GetSystemStatus', argument);
+export const GetSetting = async (argument: iptv.SettingDto): Promise<iptv.SettingDto | null> => invokeHubConnection<iptv.SettingDto>('GetSetting', argument);
+export const GetSystemStatus = async (argument: iptv.SdSystemStatus): Promise<iptv.SdSystemStatus | null> =>
+  invokeHubConnection<iptv.SdSystemStatus>('GetSystemStatus', argument);
 export const LogIn = async (argument: iptv.LogInRequest): Promise<void | null> => {
   await invokeHubConnection<void>('LogIn', argument);
 };
