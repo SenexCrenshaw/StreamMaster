dcab35664bdf06e0531946e095e6a917caccb1de
SenexCrenshaw <35600301+SenexCrenshaw@users.noreply.github.com>
2024-02-29 15:35:07 -0500

    fix ep

--- entrypoint.sh
+++ entrypoint.sh
@@ -191,10 +191,6 @@ else
     exit 1
 fi
 
-#PGADMIN_PLATFORM_TYPE=$PGADMIN_PLATFORM_TYPE PGADMIN_SETUP_EMAIL=$PGADMIN_SETUP_EMAIL PGADMIN_SETUP_PASSWORD=$PGADMIN_SETUP_PASSWORD /usr/pgadmin4/bin/setup-web.sh --yes
-#service postgresql start
-#service apache2 start
-
 # Execute the main application as the specified user
 if [ "$PUID" -ne 0 ] && [ "$PGID" -ne 0 ]; then
     echo "Running as $user_name:$group_name"
