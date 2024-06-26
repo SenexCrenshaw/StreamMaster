790676598b03bc0d40219562cc39ce422c15f436
SenexCrenshaw <35600301+SenexCrenshaw@users.noreply.github.com>
2024-06-25 20:45:52 -0400

    a

--- streammasterwebui/features/settings/About.tsx
+++ streammasterwebui/features/settings/About.tsx
@@ -1,14 +1,20 @@
 import SMPopUp from '@components/sm/SMPopUp';
+import { useSMContext } from '@lib/signalr/SMProvider';
 import { memo } from 'react';
 
 export interface SChannelMenuProperties {}
 
 const About = () => {
+  const { settings } = useSMContext();
   return (
     <SMPopUp showClose={false} title="About" info="" placement="bottom" icon="pi-question" buttonClassName="icon-yellow" contentWidthSize="2">
       <div className="flex flex-column sm-center-stuff">
+        <div className="layout-padding-bottom" />
         <img className="sm-w-2" alt="Stream Master Logo" src="/images/SMNewX32.png" />
         Stream Master
+        <div className="col-6 m-0 p-0 justify-content-center align-content-start text-xs text-center">
+          <div className="sm-text-xs">{settings.Release ?? ''}</div>
+        </div>
         <div className="flex  sm-center-stuff">
           <div className="flex flex-column w-full">
             <div className="layout-padding-bottom" />
@@ -19,6 +25,7 @@ const About = () => {
             <div className="flex ustify-content-start align-items-center">
               <img className="sm-w-2rem" alt="Stream Master Logo" src="/images/SenexBig.png" />- Senex Crenshaw - Dev
             </div>
+
             <div className="layout-padding-bottom" />
           </div>
         </div>
