9a03bb0c14bfc8dc864e45b210c6b63010b43529
SenexCrenshaw <35600301+SenexCrenshaw@users.noreply.github.com>
2024-10-17 20:15:53 -0400

    logdipslay fdix

--- streammasterwebui/features/settings/LogDisplay.tsx
+++ streammasterwebui/features/settings/LogDisplay.tsx
@@ -1,7 +1,6 @@
-import { memo, useMemo } from 'react';
+import { memo } from 'react';
 import useGetLogContents from '@lib/smAPI/Logs/useGetLogContents';
 import { ScrollPanel } from 'primereact/scrollpanel';
-import { JsonEditor } from 'json-edit-react';
 
 interface LogDisplayProperties {
   readonly logName: string;
@@ -11,15 +10,11 @@ const LogDisplay = ({ logName }: LogDisplayProperties) => {
   const query = useGetLogContents({ logName });
 
   const parsedJsonString = query.data
-    .replace(/\\u0022/g, '"') // Replace unicode for double quotes
-    .replace(/\\r\\n/g, '\r\n') // Replace escaped newlines
-    .replace(/\\\\/g, '\\'); // Replace escaped backslashes
-
-  const getContent = useMemo(() => {
-    if (!query.data) return <div>Loading...</div>;
-    let jsonObject = JSON.parse(query.data);
-    return <JsonEditor data={jsonObject} restrictEdit restrictDelete restrictAdd theme="githubDark" />;
-  }, [query.data]);
+    ? query.data
+        .replace(/\\u0022/g, '"') // Replace unicode for double quotes
+        .replace(/\\r\\n/g, '\r\n') // Replace escaped newlines
+        .replace(/\\\\/g, '\\')
+    : ''; // Replace escaped backslashes
 
   if (query?.data === undefined || query.isLoading) {
     return <div>Loading...</div>;
