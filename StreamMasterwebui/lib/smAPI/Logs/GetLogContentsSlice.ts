b83d73cff1eba9677d218052070482b636759034
SenexCrenshaw <35600301+SenexCrenshaw@users.noreply.github.com>
2024-10-24 12:19:47 -0400

    code cleanup

---
+++ StreamMasterwebui/lib/smAPI/Logs/GetLogContentsSlice.ts
@@ -0,0 +1,112 @@
+import { PayloadAction, createSlice } from '@reduxjs/toolkit';
+import { Logger } from '@lib/common/logger';
+import {FieldData,  } from '@lib/smAPI/smapiTypes';
+import { fetchGetLogContents } from '@lib/smAPI/Logs/GetLogContentsFetch';
+
+
+interface QueryState {
+  data: Record<string, string | undefined>;
+  error: Record<string, string | undefined>;
+  isError: Record<string, boolean>;
+  isForced: boolean;
+  isLoading: Record<string, boolean>;
+}
+
+const initialState: QueryState = {
+  data: {},
+  error: {},
+  isError: {},
+  isForced: false,
+  isLoading: {}
+};
+
+const getLogContentsSlice = createSlice({
+  initialState,
+  name: 'GetLogContents',
+  reducers: {
+    clear: (state) => {
+      state = initialState;
+      Logger.debug('GetLogContents clear');
+    },
+
+    clearByTag: (state, action: PayloadAction<{ tag: string }>) => {
+      const tag = action.payload.tag;
+      for (const key in state.data) {
+        if (key.includes(tag)) {
+          state.data[key] = undefined;
+        }
+      }
+      Logger.debug('GetLogContents clearByTag');
+    },
+
+    setField: (state, action: PayloadAction<{ fieldData: FieldData }>) => {
+      const { fieldData } = action.payload;
+
+      if (fieldData.Entity !== undefined && state.data[fieldData.Id]) {
+        state.data[fieldData.Id] = fieldData.Value;
+        return;
+      }
+      Logger.debug('GetLogContents setField');
+    },
+    setIsForced: (state, action: PayloadAction<{ force: boolean }>) => {
+      const { force } = action.payload;
+      state.isForced = force;
+
+      const updatedData = { ...state.data };
+      for (const key in updatedData) {
+        if (updatedData[key]) {
+          updatedData[key] = undefined;
+        }
+      }
+      state.data = updatedData;
+      Logger.debug('GetLogContents  setIsForced ', force);
+    },
+    setIsLoading: (state, action: PayloadAction<{ param: string; isLoading: boolean }>) => {
+      const { param, isLoading } = action.payload;
+      if (param !== undefined) {
+        const paramString = JSON.stringify(param);
+        state.isLoading[paramString] = isLoading;
+      } else {
+        for (const key in state.data) {
+          state.isLoading[key] = action.payload.isLoading;
+        }
+      }
+      Logger.debug('GetLogContents setIsLoading ', action.payload.isLoading);
+    }
+  },
+
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchGetLogContents.pending, (state, action) => {
+        const paramString = JSON.stringify(action.meta.arg);
+        state.isLoading[paramString] = true;
+        state.isError[paramString] = false;
+        state.isForced = false;
+        state.error[paramString] = undefined;
+      })
+      .addCase(fetchGetLogContents.fulfilled, (state, action) => {
+        if (action.payload) {
+          const { param, value } = action.payload;
+          const paramString = JSON.stringify(param);
+          state.data[paramString] = value;
+          setIsLoading({ isLoading: false, param: paramString });
+          state.isLoading[paramString] = false;
+          state.isError[paramString] = false;
+          state.error[paramString] = undefined;
+          state.isForced = false;
+        }
+      })
+      .addCase(fetchGetLogContents.rejected, (state, action) => {
+        const paramString = JSON.stringify(action.meta.arg);
+        state.error[paramString] = action.error.message || 'Failed to fetch';
+        state.isError[paramString] = true;
+        setIsLoading({ isLoading: false, param: paramString });
+        state.isLoading[paramString] = false;
+        state.isForced = false;
+      });
+
+  }
+});
+
+export const { clear, clearByTag, setIsLoading, setIsForced, setField } = getLogContentsSlice.actions;
+export default getLogContentsSlice.reducer;
