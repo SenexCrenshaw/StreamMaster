e5b0f7c96f2f853732b4f07009a1a833603e136d
SenexCrenshaw <35600301+SenexCrenshaw@users.noreply.github.com>
2024-10-31 14:00:47 -0400

    feat: POSTGRES_USER_FILE and POSTGRES_PASSWORD_FILE

--- env.sh
+++ env.sh
@@ -8,22 +8,37 @@ default_host="127.0.0.1"
 default_set_perms=1
 default_backup_versions_to_keep=5
 
+# Read user and password from files if available
+if [ -f "${POSTGRES_USER_FILE}" ]; then
+    POSTGRES_USER=$(cat "${POSTGRES_USER_FILE}")
+else
+    POSTGRES_USER="${POSTGRES_USER:-$default_dbuser}"
+fi
+
+if [ -f "${POSTGRES_PASSWORD_FILE}" ]; then
+    POSTGRES_PASSWORD=$(cat "${POSTGRES_PASSWORD_FILE}")
+else
+    POSTGRES_PASSWORD="${POSTGRES_PASSWORD:-$default_dbpassword}"
+fi
+
 # Set and export environment variables with defaults if they are not set
 export PGADMIN_SETUP_EMAIL="${PGADMIN_SETUP_EMAIL:-$default_email}" 
 export PGADMIN_SETUP_PASSWORD="${PGADMIN_SETUP_PASSWORD:-$default_password}" 
 export PGADMIN_PLATFORM_TYPE=debian
-export POSTGRES_USER="${POSTGRES_USER:-$default_dbuser}" 
-export POSTGRES_PASSWORD="${POSTGRES_PASSWORD:-$default_dbpassword}" 
+export POSTGRES_USER
+export POSTGRES_PASSWORD
 export PGDATA=/config/DB
 export POSTGRES_HOST="${POSTGRES_HOST:-$default_host}" 
 export POSTGRES_DB="${POSTGRES_DB:-$default_db}" 
-export POSTGRES_SET_PERMS="${POSTGRES_SET_PERMS:-$default_set_perms}" 
+export POSTGRES_SET_PERMS="${POSTGRES_SET_PERMS:-$default_set_perms}"
 
+# Function to determine if host is local
 is_local() {
     [[ "$1" == "127.0.0.1" || "$1" == "localhost" ]] && echo "1" || echo "0"
 }
 export POSTGRES_ISLOCAL=$(is_local "$POSTGRES_HOST")
 
+# Backup and restore configurations
 export BACKUP_VERSIONS_TO_KEEP="${BACKUP_VERSIONS_TO_KEEP:-$default_backup_versions_to_keep}" 
 export PATH=$PATH:/usr/lib/postgresql/15/bin
 export BACKUP_DIR=/config/Backups
