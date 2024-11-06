afcb7eaf132d5811d696d7d5a074bf6e0231ed76
SenexCrenshaw <35600301+SenexCrenshaw@users.noreply.github.com>
2024-11-06 10:52:11 -0500

    fix: env.sh default_dbpassword

--- env.sh
+++ env.sh
@@ -8,32 +8,32 @@ default_host="127.0.0.1"
 default_set_perms=1
 
 # Handling POSTGRES_USER and POSTGRES_USER_FILE
-if [ -f "${POSTGRES_USER_FILE}" ]; then        
+if [ -f "${POSTGRES_USER_FILE}" ]; then
     export POSTGRES_USER=$(cat "${POSTGRES_USER_FILE}")
-    
-else    
+
+else
     export POSTGRES_USER="${POSTGRES_USER:-$default_dbuser}"
 fi
 unset POSTGRES_USER_FILE
 
 # Handling POSTGRES_PASSWORD and POSTGRES_PASSWORD_FILE
-if [ -f "${POSTGRES_PASSWORD_FILE}" ]; then        
+if [ -f "${POSTGRES_PASSWORD_FILE}" ]; then
     export POSTGRES_PASSWORD=$(cat "${POSTGRES_PASSWORD_FILE}")
-    
-else    
-    export POSTGRES_PASSWORD="${POSTGRES_USER:-$default_dbuser}"
+
+else
+    export POSTGRES_PASSWORD="${POSTGRES_USER:-$default_dbpassword}"
 fi
 unset POSTGRES_PASSWORD_FILE
 
 # Set and export environment variables with defaults if they are not set
-export PGADMIN_SETUP_EMAIL="${PGADMIN_SETUP_EMAIL:-$default_email}" 
-export PGADMIN_SETUP_PASSWORD="${PGADMIN_SETUP_PASSWORD:-$default_password}" 
+export PGADMIN_SETUP_EMAIL="${PGADMIN_SETUP_EMAIL:-$default_email}"
+export PGADMIN_SETUP_PASSWORD="${PGADMIN_SETUP_PASSWORD:-$default_password}"
 export PGADMIN_PLATFORM_TYPE=debian
 # export POSTGRES_USER
 # export POSTGRES_PASSWORD
 export PGDATA=/config/DB
-export POSTGRES_HOST="${POSTGRES_HOST:-$default_host}" 
-export POSTGRES_DB="${POSTGRES_DB:-$default_db}" 
+export POSTGRES_HOST="${POSTGRES_HOST:-$default_host}"
+export POSTGRES_DB="${POSTGRES_DB:-$default_db}"
 export POSTGRES_SET_PERMS="${POSTGRES_SET_PERMS:-$default_set_perms}"
 
 # Function to determine if host is local
