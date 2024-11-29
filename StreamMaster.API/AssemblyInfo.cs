94fa89d1d2571a5b612bfd934e8a502bd5a20ad8
Senex Crenshaw <senexcrenshaw@gmail.com>
2024-11-29 14:26:33 -0500

    version

--- StreamMaster.API/AssemblyInfo.cs
+++ StreamMaster.API/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.17.10.9.6")]
-[assembly: AssemblyFileVersion("0.17.10.9.6")]
+[assembly: AssemblyVersion("0.17.10.96")]
+[assembly: AssemblyFileVersion("0.17.10.96")]
 [assembly: AssemblyInformationalVersion("0.17.10-net9.6.Sha.2cb5aab4b9cab76849775c78d91a65e035522741")]
